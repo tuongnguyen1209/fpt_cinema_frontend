e40c60a2462cacff1390e32dbd9c8a097a147f2a
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-12-01 12:45:21 +0700

    fix

--- src/serivces/ticket.service.js
+++ src/serivces/ticket.service.js
@@ -2,19 +2,15 @@ import axiosClient from "./axios.client";
 const url = "/v1.php/ticket";
 
 const TicketService = {
-    createTicket: (data) => {
-      return axiosClient.post("/v1.php/ticket", data);
-    },
-    getTicketByUser: (idUser) => {
-        return axiosClient.get(`/v1.php/ticket?code=${idUser}&type=id_user`);
-    },
-    getAll: (params) => {
-      return axiosClient.get(url, { params });
-    },
-
+  createTicket: (data) => {
+    return axiosClient.post("/v1.php/ticket", data);
+  },
+  getTicketByUser: (idUser) => {
+    return axiosClient.get(`/v1.php/ticket?code=${idUser}&type=id_user`);
+  },
   getAll: (params) => {
     return axiosClient.get(url, { params });
   },
 };
 
-export default ticketService;
+export default TicketService;
