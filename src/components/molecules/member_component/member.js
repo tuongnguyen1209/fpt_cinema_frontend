3304ec2ca634408d40994b6eba996b861ba3fa7a
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-12-10 09:26:07 +0700

    add conform

--- src/components/molecules/member_component/member.js
+++ src/components/molecules/member_component/member.js
@@ -3,102 +3,147 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Title from "../title_component/title";
 import { MemberStyle } from "./memberStyle";
-import { Image } from 'antd';
+import { Image } from "antd";
 
 const Member = () => {
+  // đường dẫn từng trang
+  const routes = [
+    {
+      path: "/",
+      breadcrumbName: "Trang Chủ",
+    },
+    {
+      path: "member",
+      breadcrumbName: "Thành viên",
+    },
+    {
+      path: "chinh-sach",
+      breadcrumbName: "Chính sách",
+    },
+  ];
+  function itemRender(route, params, routes, paths) {
+    const last = routes.indexOf(route) === routes.length - 1;
+    return last ? (
+      <span>{route.breadcrumbName}</span>
+    ) : (
+      <Link to={paths.join("/")}>{route.breadcrumbName}</Link>
+    );
+  }
+  // end đường dẫn từng trang
 
-    // đường dẫn từng trang
-    const routes = [
-        {
-          path: '/',
-          breadcrumbName: 'Trang Chủ',
-        },
-        {
-          path: 'member',
-          breadcrumbName: 'Thành viên',
-        },
-        {
-          path: 'chinh-sach',
-          breadcrumbName: 'Chính sách',
-        },
-      ];
-      function itemRender(route, params, routes, paths) {
-        const last = routes.indexOf(route) === routes.length - 1;
-        return last ? (
-          <span>{route.breadcrumbName}</span>
-        ) : (
-          <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
-        );
-      }
-      // end đường dẫn từng trang
-
-    return (
-        <MemberStyle>
-            <div className="container_custom">
-                <div className="main_member">
-                    <Breadcrumb itemRender={itemRender} routes={routes} />
-                    <Title title1={"THỂ LỆ"} title2={"QUYỀN LỢI"}/>
-                    <div>
-                        <div className="div-container top-container">
-                            <h4>THỂ LỆ</h4>
-                            <p>Chương trình khách hàng thân thiết Galaxy là chương trình ưu đãi dựa trên điểm tích lũy của các thành viên gồm Star, G-star, X-star.
-                            Với mỗi giao dịch tại hệ thống rạp Galaxy, bạn sẽ nhận được điểm thưởng tương ứng. Hình thức tích lũy như sau:</p>
-                            <div className="div-img"><Image
-                                width={600}
-                                src="
+  return (
+    <MemberStyle>
+      <div className="container_custom">
+        <div className="main_member">
+          <Breadcrumb itemRender={itemRender} routes={routes} />
+          <Title title1={"THỂ LỆ"} title2={"QUYỀN LỢI"} />
+          <div>
+            <div className="div-container top-container">
+              <h4>THỂ LỆ</h4>
+              <p>
+                Chương trình khách hàng thân thiết Galaxy là chương trình ưu đãi
+                dựa trên điểm tích lũy của các thành viên gồm Star, G-star,
+                X-star. Với mỗi giao dịch tại hệ thống rạp Galaxy, bạn sẽ nhận
+                được điểm thưởng tương ứng. Hình thức tích lũy như sau:
+              </p>
+              <div className="div-img">
+                <Image
+                  width={600}
+                  src="
                                 https://www.galaxycine.vn/media/2019/12/18/10-000d1star_1576655356732.jpg"
-                            /></div>
-                        </div>
-
-                        <div className="div-container">
-                            <h4>Cách làm tròn điểm thưởng: </h4>
-                            <p>Từ 0.1 đến 0.4: làm tròn xuống (Ví dụ: 3.2 sao sẽ được tích vào tài khoản 3 sao)<br></br>
-                            Từ 0.5 đến 0.9: làm tròn lên (Ví dụ: 3.5 sao sẽ được tích vào tài khoản 4 sao)</p>
-                        </div>
-
-                        <div className="div-container">
-                            <h4>Cấp độ thành viên</h4>
-                            <div className="div-img img2"><Image
-                                width={600}
-                                src="
-                                https://www.galaxycine.vn/media/2019/12/18/thanh-tinh-diem_1576655615185.jpg"
-                            /></div>
-                            <p><b>Star</b> là thành viên thân thiết có tổng chi tiêu trong năm dưới 2,000,000 đồng tính từ ngày 1/1-31/12. <br></br>
-                            <b>G-star</b> là thành viên thân thiết có tổng chi tiêu trong năm từ 2,000,000 đồng đến 3,999,999 đồng tính từ ngày 1/1-31/12.<br></br>
-                            <b>X-star</b> là thành viên thân thiết có tổng chi tiêu từ 4,000,000 đồng trở lên tính từ ngày 1/1-31/12</p>
-                        </div>
-
-                        <div className="div-container">
-                            <h4>Chính sách đổi quà</h4>
-                            <div className="div-img img3"><Image
-                                width={600}
-                                src="
-                                https://www.galaxycine.vn/media/2019/12/18/cs-doi-qua_1576655681445.jpg"
-                            /></div>
-                        </div>
-
-                        <div className="div-container">
-                            <h4>Lưu ý:</h4>
-                            <ul>
-                                <li>Thông tin định danh thành viên gồm có email và số điện thoại bắt buộc phải hợp lệ</li>
-                                <li>Email không hợp lệ là email không có thực tại thời điểm Galaxy Cinema rà soát dữ liệu thành viên.</li>
-                                <li>Số điện thoại không hợp lệ là số điện thoại không liên lạc được hoặc số điện thoại không thuộc sở hữu của chủ tài khoản thành viên ở thời điểm Galaxy Cinema rà soát dữ liệu thành viên</li>
-                                <li>Với các trường hợp không hợp lệ, Galaxy Cinema có quyền xóa tài khoản thành viên mà không cần thông báo trước.</li>
-                                <li>Tài khoản thành viên không có đủ thông tin định danh gồm email và số điện thoại hợp lệ, Galaxy Cinema có quyền xóa tài khoản thành viên mà không cần thông báo trước.</li>
-                                <li>Điểm tích lũy có giá trị áp dụng tại tất cả các rạp Galaxy Cinema trên toàn quốc.</li>
-                                <li>Điểm tích lũy có thời hạn sử dụng là 01 năm (tính từ ngày 01/01/2021-31/12/2021).</li>
-                                <li>Điểm tích lũy sẽ bị trừ sau mỗi lần đổi quà.</li>
-                                <li>Không giới hạn số lượng quà tặng được đổi.</li>
-                                <li>Bạn có thể dễ dàng kiểm tra điểm tích lũy của mình trên Website Galaxy Cinema hoặc Ứng dụng GLX trên điện thoại (Mobile App).</li>
-
-                            </ul>
-                        </div>
-
-                    </div>
-                </div> 
+                />
+              </div>
             </div>
-        </MemberStyle>
-    )
-}
+
+            <div className="div-container">
+              <h4>Cách làm tròn điểm thưởng: </h4>
+              <p>
+                Từ 0.1 đến 0.4: làm tròn xuống (Ví dụ: 3.2 sao sẽ được tích vào
+                tài khoản 3 sao)<br></br>
+                Từ 0.5 đến 0.9: làm tròn lên (Ví dụ: 3.5 sao sẽ được tích vào
+                tài khoản 4 sao)
+              </p>
+            </div>
+
+            <div className="div-container">
+              <h4>Cấp độ thành viên</h4>
+              <div className="div-img img2">
+                <Image
+                  width={600}
+                  src="
+                                https://www.galaxycine.vn/media/2019/12/18/thanh-tinh-diem_1576655615185.jpg"
+                />
+              </div>
+              <p>
+                <b>Star</b> là thành viên thân thiết có tổng chi tiêu trong năm
+                dưới 2,000,000 đồng tính từ ngày 1/1-31/12. <br></br>
+                <b>G-star</b> là thành viên thân thiết có tổng chi tiêu trong
+                năm từ 2,000,000 đồng đến 3,999,999 đồng tính từ ngày 1/1-31/12.
+                <br></br>
+                <b>X-star</b> là thành viên thân thiết có tổng chi tiêu từ
+                4,000,000 đồng trở lên tính từ ngày 1/1-31/12
+              </p>
+            </div>
+
+            <div className="div-container">
+              <h4>Chính sách đổi quà</h4>
+              <div className="div-img img3">
+                <Image
+                  width={600}
+                  src="
+                                https://www.galaxycine.vn/media/2019/12/18/cs-doi-qua_1576655681445.jpg"
+                />
+              </div>
+            </div>
+
+            <div className="div-container">
+              <h4>Lưu ý:</h4>
+              <ul>
+                <li>
+                  Thông tin định danh thành viên gồm có email và số điện thoại
+                  bắt buộc phải hợp lệ
+                </li>
+                <li>
+                  Email không hợp lệ là email không có thực tại thời điểm Galaxy
+                  Cinema rà soát dữ liệu thành viên.
+                </li>
+                <li>
+                  Số điện thoại không hợp lệ là số điện thoại không liên lạc
+                  được hoặc số điện thoại không thuộc sở hữu của chủ tài khoản
+                  thành viên ở thời điểm Galaxy Cinema rà soát dữ liệu thành
+                  viên
+                </li>
+                <li>
+                  Với các trường hợp không hợp lệ, Galaxy Cinema có quyền xóa
+                  tài khoản thành viên mà không cần thông báo trước.
+                </li>
+                <li>
+                  Tài khoản thành viên không có đủ thông tin định danh gồm email
+                  và số điện thoại hợp lệ, Galaxy Cinema có quyền xóa tài khoản
+                  thành viên mà không cần thông báo trước.
+                </li>
+                <li>
+                  Điểm tích lũy có giá trị áp dụng tại tất cả các rạp Galaxy
+                  Cinema trên toàn quốc.
+                </li>
+                <li>
+                  Điểm tích lũy có thời hạn sử dụng là 01 năm (tính từ ngày
+                  01/01/2021-31/12/2021).
+                </li>
+                <li>Điểm tích lũy sẽ bị trừ sau mỗi lần đổi quà.</li>
+                <li>Không giới hạn số lượng quà tặng được đổi.</li>
+                <li>
+                  Bạn có thể dễ dàng kiểm tra điểm tích lũy của mình trên
+                  Website Galaxy Cinema hoặc Ứng dụng GLX trên điện thoại
+                  (Mobile App).
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </MemberStyle>
+  );
+};
 
 export default Member;
--- src/views/site/page_info_transaction/info_transaction.js
+++ src/views/site/page_info_transaction/info_transaction.js
@@ -10,207 +10,222 @@ import TicketService from "../../../serivces/ticket.service";
 import { formatPrice } from "../../../ultil/format";
 
 const PageTransactionStyle = styled.div`
-    .example {
+  .example {
     margin: 20px 0;
     margin-bottom: 20px;
     padding: 30px 50px;
     text-align: center;
     background: transparent;
     border-radius: 4px;
+  }
+  .container_custom_transaction {
+    width: 100%;
+    margin-left: -1px;
+    a {
+      color: blue;
     }
-    .container_custom_transaction {
-        width: 100%;
-        margin-left: -1px;
-        a {
-            color: blue;
-        }
-        table {
-            width: 100%;
-        }
-        th {
-            background-color: #2c3e50;
-            color: white;
-            border-left: 0.5px solid rgba(0,0,0,.4);
-            padding: 10px;
-            width: auto;
-            text-align: center;
-        }
-        tr {
-            height: 60px;
-        }
-        tr:nth-child(even) {
-            background-color: rgba(189, 195, 199,0.3);
-        }
-        td {
-            font-size: 0.8rem;
-            padding: 10px;
-            text-align: center;
-        }
-        .paid {
-            font-size: 0.7rem;
-            background-color: gray;
-            color: white;
-            padding: 5px;
-            width: 120px;
-            border: none;
-            outline: none;
-            cursor: pointer;
-        }
-        .unpaid {
-            font-size: 0.7rem;
-            background-color: red;
-            color: white;
-            padding: 5px;
-            width: 120px;
-            border: none;
-            outline: none;
-            cursor: pointer;
-        }
-        .unpaid:hover {
-            opacity: 0.8;
-        }
+    table {
+      width: 100%;
     }
-`
+    th {
+      background-color: #2c3e50;
+      color: white;
+      border-left: 0.5px solid rgba(0, 0, 0, 0.4);
+      padding: 10px;
+      width: auto;
+      text-align: center;
+    }
+    tr {
+      height: 60px;
+    }
+    tr:nth-child(even) {
+      background-color: rgba(189, 195, 199, 0.3);
+    }
+    td {
+      font-size: 0.8rem;
+      padding: 10px;
+      text-align: center;
+    }
+    .paid {
+      font-size: 0.7rem;
+      background-color: gray;
+      color: white;
+      padding: 5px;
+      width: 120px;
+      border: none;
+      outline: none;
+      cursor: pointer;
+    }
+    .unpaid {
+      font-size: 0.7rem;
+      background-color: red;
+      color: white;
+      padding: 5px;
+      width: 120px;
+      border: none;
+      outline: none;
+      cursor: pointer;
+    }
+    .unpaid:hover {
+      opacity: 0.8;
+    }
+  }
+`;
 const PageTransaction = () => {
+  const [spin, setSpin] = useState(true);
 
-    const [spin,setSpin] = useState(true);
+  // const [listMovie, setListMovie] = useState([]);
+  // const [idMovie, setIdMovie] = useState();
 
-    // const [listMovie, setListMovie] = useState([]);
-    // const [idMovie, setIdMovie] = useState();
+  // useEffect(() => {
 
-    // useEffect(() => {
+  //     const fetchMovieList = async () => {
+  //         try {
+  //           const response = await MovieService.getAllMovie();
+  //         //   console.log(response);
+  //           setListMovie(response.movie);
+  //         }catch (error) {
+  //           console.log("Failed to fetch movie list: ",error);
+  //         }
+  //       }
+  //       fetchMovieList();
 
-    //     const fetchMovieList = async () => {
-    //         try {
-    //           const response = await MovieService.getAllMovie();
-    //         //   console.log(response);
-    //           setListMovie(response.movie);
-    //         }catch (error) {
-    //           console.log("Failed to fetch movie list: ",error);
-    //         }
-    //       }
-    //       fetchMovieList();
+  // },[])
 
-    // },[])
+  // const handleMoveDetail = (nameMovie) => {
+  //     for(let i = 0; i <= listMovie.length; i++) {
+  //         if(listMovie[i]?.name === nameMovie.text) {
+  //             // console.log(listMovie[i]?.name + " === " + nameMovie.text);
+  //             // console.log(listMovie[i]?.id_movie)
+  //             setIdMovie(listMovie[i]?.id_movie)
+  //         }
+  //         else {
+  //             console.log("KHÁC")
+  //         }
+  //     }
+  // }
 
-    // const handleMoveDetail = (nameMovie) => {
-    //     for(let i = 0; i <= listMovie.length; i++) {
-    //         if(listMovie[i]?.name === nameMovie.text) {
-    //             // console.log(listMovie[i]?.name + " === " + nameMovie.text);
-    //             // console.log(listMovie[i]?.id_movie)
-    //             setIdMovie(listMovie[i]?.id_movie)
-    //         }
-    //         else {
-    //             console.log("KHÁC")
-    //         }
-    //     }
-    // } 
-    
-    // login
-    const idUserReducer = useSelector((state) => state.user);
-    const idUser = idUserReducer.user.id_user;
+  // login
+  const idUserReducer = useSelector((state) => state.user);
+  const idUser = idUserReducer.user.id_user;
 
-    const [billTicket,setBillTicket] = useState([]);
-    const [nameRoom,setNameRoom] = useState([]);
-    const [idTicket, setIdTicket] = useState();
-    const [linkPayment,setLinkPayment] = useState();
+  const [billTicket, setBillTicket] = useState([]);
+  const [nameRoom, setNameRoom] = useState([]);
+  const [idTicket, setIdTicket] = useState();
+  const [linkPayment, setLinkPayment] = useState();
 
-    
-
-    useEffect(() => {
+  useEffect(() => {
     const fetchTransactionUser = async () => {
-        
-            try {
-                const response = await TicketService.getTicketByUser(idUser);
-                // console.log(response.data.ticket);
-                setBillTicket(response.data.ticket);
-                setSpin(false);
-            }catch (error) {
-                console.log("Failed to fetch id user: ",error);
-            }
-        }
-        fetchTransactionUser();
+      try {
+        const response = await TicketService.getTicketByUser(idUser);
+        // console.log(response.data.ticket);
+        setBillTicket(response.data.ticket);
+        setSpin(false);
+      } catch (error) {
+        console.log("Failed to fetch id user: ", error);
+      }
+    };
+    fetchTransactionUser();
 
-        const fetchLinkPayment = async () => {
-            try {
-                const response = await PaymentService.getLinkPayment(idTicket);
-                // console.log(response);
-                setLinkPayment(response.payment.data);
-            }catch (error) {
-                console.log("Failed to fetch link payment: ",error);
-            }
-        }
-        fetchLinkPayment();
+    const fetchLinkPayment = async () => {
+      try {
+        const response = await PaymentService.getLinkPayment(idTicket);
+        // console.log(response);
+        setLinkPayment(response.payment.data);
+      } catch (error) {
+        console.log("Failed to fetch link payment: ", error);
+      }
+    };
+    fetchLinkPayment();
 
-        
-        
-        axios.get('https://cinemafptproject.herokuapp.com/v1.php/room')
-        .then(function (response) {
-            // console.log(response.data.data.room);
+    axios
+      .get("https://cinemafptproject.herokuapp.com/v1.php/room")
+      .then(function (response) {
+        // console.log(response.data.data.room);
         setNameRoom(response.data.data.room);
-        })
-        .catch(function (error) {
+      })
+      .catch(function (error) {
         console.log(error);
-    })  
-    },[idTicket,idUser])
-    
-    const handleGetIdTicket = (id_ticket) => {
-        setIdTicket(id_ticket);
-        console.log(id_ticket);
-    }
-    
-    return (  
-        <PageTransactionStyle>
-            <div className="container_custom_transaction">
-            <table>
-                <tr>
-                    <th>Khách Hàng</th>
-                    <th>Mã Vé</th>
-                    <th>Phim</th>
-                    <th>Ngày Chiếu</th>
-                    <th>Giờ Chiếu</th>
-                    <th>Phòng Chiếu</th>
-                    <th>Ghế</th>
-                    <th>Combo</th>
-                    <th>Thông Tin Vé</th>
-                    <th>Trạng Thái</th>
-                    <th>Tổng Tiền</th>
-                </tr>
-                {billTicket.map((item,index) => (
-                    <tr key={index}>
-                        <td>{item?.full_name}</td>
-                        <td>{item?.id_ticket}</td>
-                        <td>{item?.name_mv}</td>
-                        <td>{item?.date}</td>
-                        <td>{item?.time_start}</td>
-                        <td>
-                            {nameRoom.map(idRoom => (
-                                <span>{idRoom.id_room === item.id_room ? idRoom.name_room : ""}</span>    
-                            ))}
-                        </td>
-                        <td>
-                            {item?.seat.map((seat,index) => (
-                                <span>{seat.seat}, </span>
-                            ))}
-                        </td>
-                        <td>
-                            {item?.combo.map((combo,index) => (
-                                <span key={index}>{combo.combo}({combo.quantity}), </span>
-                            ))}
-                        </td>
-                        <td>{item?.ticket_information}</td>
-                        <td>{item?.status === '1' ? <button className="paid">Đã Thanh Toán</button> : <button className="unpaid" onClick={() => handleGetIdTicket(item.id_ticket)}><a href={linkPayment} style={{color: "white"}}>Chưa Thanh Toán</a></button>}</td>
-                        <td>{formatPrice(item?.Total_money)}</td>
-                    </tr>
+      });
+  }, [idTicket, idUser]);
+
+  const handleGetIdTicket = (id_ticket) => {
+    setIdTicket(id_ticket);
+    console.log(id_ticket);
+  };
+
+  return (
+    <PageTransactionStyle>
+      <div className="container_custom_transaction">
+        <table>
+          <tr>
+            <th>Khách Hàng</th>
+            <th>Mã Vé</th>
+            <th>Phim</th>
+            <th>Ngày Chiếu</th>
+            <th>Giờ Chiếu</th>
+            <th>Phòng Chiếu</th>
+            <th>Ghế</th>
+            <th>Combo</th>
+            <th>Thông Tin Vé</th>
+            <th>Trạng Thái</th>
+            <th>Tổng Tiền</th>
+          </tr>
+          {billTicket.map((item, index) => (
+            <tr key={index}>
+              <td>{item?.full_name}</td>
+              <td>{item?.id_ticket}</td>
+              <td>{item?.name_mv}</td>
+              <td>{item?.date}</td>
+              <td>{item?.time_start}</td>
+              <td>
+                {nameRoom.map((idRoom) => (
+                  <span>
+                    {idRoom.id_room === item.id_room ? idRoom.name_room : ""}
+                  </span>
                 ))}
-                </table>
-            </div>
-            <Title title1={"THÔNG TIN THÀNH VIÊN"} title2={"GIAO DỊCH CỦA TÔI"} />
-            <div className="example">
-                <Spin spinning={spin} size="large"/>
-            </div>,
-        </PageTransactionStyle>
-    );
-}
+              </td>
+              <td>
+                {item?.seat.map((seat, index) => (
+                  <span>{seat.seat}, </span>
+                ))}
+              </td>
+              <td>
+                {item?.combo.map((combo, index) => (
+                  <span key={index}>
+                    {combo.combo}({combo.quantity}),{" "}
+                  </span>
+                ))}
+              </td>
+              <td>{item?.ticket_information}</td>
+              <td>
+                {item?.status === "1" && (
+                  <button className="paid">Đã Thanh Toán</button>
+                )}
+                {item?.status === "0" && (
+                  <button
+                    className="unpaid"
+                    onClick={() => handleGetIdTicket(item.id_ticket)}
+                  >
+                    <a href={linkPayment} style={{ color: "white" }}>
+                      Chưa Thanh Toán
+                    </a>
+                  </button>
+                )}
+                {item?.status === "2" && "đã nhận vé"}
+              </td>
+              <td>{formatPrice(item?.Total_money)}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+      <Title title1={"THÔNG TIN THÀNH VIÊN"} title2={"GIAO DỊCH CỦA TÔI"} />
+      <div className="example">
+        <Spin spinning={spin} size="large" />
+      </div>
+      ,
+    </PageTransactionStyle>
+  );
+};
 
 export default PageTransaction;
