8a9a53cee79f18655762519f000775de1c3b3631
tuongnguyen1209 <65478301+tuongnguyen1209@users.noreply.github.com>
2021-12-14 21:24:07 +0700

    fix bug

--- src/views/index.js
+++ src/views/index.js
@@ -35,9 +35,7 @@ function Views() {
       <Switch>
         <PrivateRoute
           path={ADMIN_PREFIX_PATH}
-          isAuthenticated={
-            user.isLogin && `${userdata.user.administration}` === "1"
-          }
+          isAuthenticated={user.isLogin && user.user.administration === 1}
         >
           <Admin />
         </PrivateRoute>
